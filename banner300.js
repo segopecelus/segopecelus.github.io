0bbcd8dd6c0745e5fa0f96f729265928a1e7a27c
segopecelus <102438913+segopecelus@users.noreply.github.com>
2022-11-23 05:03:10 +0700

    Add files via upload

---
+++ banner300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '63ab83c16989526b5e066846a849db42',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/63ab83c16989526b5e066846a849db42/invoke.js"></scr' + 'ipt>');
---
+++ banner728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9f0de11e5fd48835de6964cc4939507c',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/9f0de11e5fd48835de6964cc4939507c/invoke.js"></scr' + 'ipt>');
